39578ee899422a843ceff1663627ae0e61ed3cd4
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-10 17:34:18 +0530

    added fileupload npm module

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var userRouter = require('./routes/user'); 
 var adminRouter = require('./routes/admin'); 
 
 var { create } = require("express-handlebars");
 var app = express();
-
+var fileUpload = require('express-fileupload');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 
 app.use('/', userRouter);
