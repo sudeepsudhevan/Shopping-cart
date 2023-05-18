a324a1f6b93f408c99ffac0153bdb845f007ea9b
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-18 14:13:46 +0530

    admin.js working image moving after lot of search

--- routes/admin.js
+++ routes/admin.js
@@ -1,58 +1,35 @@
 var express = require('express');
+const { render } = require('../app');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  let products = [
-    {
-      name: 'iPhone 13 Pro Max',
-      category: 'Smartphone',
-      description: "iPhone 13 Pro max with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/09/2-1-500x500.jpg",
-    },
-    {
-      name: "Oneplus 9 RT",
-      category: "smartphone",
-      description: "Oneplus 9RT with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/08/20211011145119.jpg",
-    },
-    {
-      name: "Oppo Reno 7",
-      category: "smartphone",
-      description: "Oppo Reno 7 with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2022/03/1-11-500x500.jpg",
-    },
-    {
-      name: "Samsung Galaxy S21 FE",
-      category: "smartphone",
-      description: "Samsung Galaxy S21 FE with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/06/4-500x500.jpg",
+  productHelpers.getAllProducts().then((products) => {
+    console.log(products);
+    res.render('admin/view-products', { admin: true, products });
+  });
+
+
+});
+router.get('/add-product', function (req, res) {
+  res.render('admin/add-product', { admin: true });
+
+});
+router.post('/add-product', function (req, res) {
+  let image = req.files.Image;
+  let imagePath = '/product-images/' + Date.now() + '.jpg';
+  image.mv('./public' + imagePath, (err, done) => {
+    if (!err) {
+      let product = req.body;
+      product.image = imagePath;
+      productHelpers.addProduct(product, (id) => {
+        res.render("admin/add-product", { admin: true });
+      });
+    } else {
+      console.log(err);
     }
-  ]
-
-  res.render('admin/view-products', { admin: true, products });
-});
-router.get('/add-product', function (req, res ) {
-  res.render('admin/add-product',{admin: true});
-
-});
-router.post('/add-product', function (req, res ) {
-  console.log(req.body); 
-  console.log(req.files.Image);
-
-  productHelpers.addProduct(req.body,(id)=>{
-    let image=req.files.Image;
-    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
-      if(!err){
-        res.render("admin/add-product",{admin:true})
-      }else{
-        console.log(err);
-      }
-    })
-   
-  })
-
+  });
 });
 
 module.exports = router;
