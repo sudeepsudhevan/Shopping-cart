9f15bf646c5d85e12d8d51f2b41825ec7b8c05d5
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-18 18:50:11 +0530

    user.js

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let products = [
-    {
-      name: 'iPhone 13 Pro Max',
-      category: 'Smartphone',
-      description: "iPhone 13 Pro max with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/09/2-1-500x500.jpg",
-    },
-    {
-      name: "Oneplus 9 RT",
-      category: "smartphone",
-      description: "Oneplus 9RT with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/08/20211011145119.jpg",
-    },
-    {
-      name: "Oppo Reno 7",
-      category: "smartphone",
-      description: "Oppo Reno 7 with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2022/03/1-11-500x500.jpg",
-    },
-    {
-      name: "Samsung Galaxy S21 FE",
-      category: "smartphone",
-      description: "Samsung Galaxy S21 FE with 8gb ram and 128gb storage",
-      Image: "https://www.gizmochina.com/wp-content/uploads/2021/06/4-500x500.jpg",
-    }
-  ]
-  res.render('index', { products,admin: false });
+  productHelpers.getAllProducts().then((products) => {
+    res.render('user/view-products', { admin: false, products });
+  });
+  
 });
 
 module.exports = router;
