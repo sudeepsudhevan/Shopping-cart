9527690ca3859c09332809384e20ec8e01579f76
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-19 19:57:59 +0530

    user helper password checking

---
+++ helpers/user-helpers.js
@@ -0,0 +1,42 @@
+var db = require('../config/connection');
+var collection = require('../config/collections');
+const bcrypt = require('bcrypt');
+
+
+module.exports = {
+    doSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            userData.password = await bcrypt.hash(userData.password, 10)
+            console.log(collection.USER_COLLECTIONS)
+            db.get().collection(collection.USER_COLLECTIONS).insertOne(userData).then((data) => {
+                resolve(data.insertedId)
+            }).catch((err)=>{
+                console.log(err);
+            })
+        })
+    },
+    doLogin: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            let loginStatus = false
+            let response = {}
+            let user = await db.get().collection(collection.USER_COLLECTIONS).findOne({ email: userData.email })
+            if (user) {
+                bcrypt.compare(userData.password, user.password).then((status) => {
+                    if (status) {
+                        console.log("login success");
+                        response.user = user
+                        response.status = true
+                        resolve(response)
+                    } else {
+                        console.log("login failed");
+                        resolve({ status: false })
+                    }
+                })
+            } else {
+                console.log("login failed");
+                resolve({ status: false })
+            }
+        }) 
+
+    }           
+}
