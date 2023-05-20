c53a6617a6813d9e7d5b80e6851e0c2e848b947d
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-20 18:30:31 +0530

    some things render

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,14 @@ var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers = require('../helpers/user-helpers');
 
+const verifyLogin = (req, res, next) => {
+  if(req.session.loggedIn){
+    next();
+  }else{
+    res.redirect('/login');
+  }
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) { // This is the root route
   let user = req.session.user;
@@ -14,7 +22,11 @@ router.get('/', function (req, res, next) { // This is the root route
 });
 
 router.get('/login', (req, res) => {
-  res.render('user/login'); //render is used to render the page, here it is login page
+  if(req.session.loggedIn){
+    res.redirect('/');  //redirect to home page
+  }else
+    res.render('user/login',{"loginErr":req.session.loginErr}); //render is used to render the page, here it is login page
+    req.session.loginErr = false;
 });
 router.get('/signup', (req, res) => {
   res.render('user/signup');
@@ -34,6 +46,7 @@ router.post('/login', (req, res) => {
 
       res.redirect('/');
     }else{
+      req.session.loginErr = "Invalid username or password";
       res.redirect('/login');
     }
 
@@ -43,6 +56,12 @@ router.post('/login', (req, res) => {
 router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/');
-}); 
+});
+
+router.get('/cart',verifyLogin, (req, res) => {
+  res.render('user/cart');
+});
+
+
 
 module.exports = router;
