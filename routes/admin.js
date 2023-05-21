159b746eeb66782a32831698a4c9f25e50461d6a
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-21 14:43:44 +0530

    render update product page and also image change in upload

--- routes/admin.js
+++ routes/admin.js
@@ -48,7 +48,18 @@ router.get('/delete-product/:id', (req, res) => {
 router.get('/edit-product/:id', async (req, res) => {
   let product = await productHelpers.getProductDetails(req.params.id)
   console.log(product);
-  res.render('admin/edit-product', { product, admin: true })
+  res.render('admin/edit-product', { product,admin: true });
+})
+router.post('/edit-product/:id', (req, res) => {
+  console.log(req.params.id);
+  productHelpers.updateProduct(req.params.id, req.body).then(() => {
+    res.redirect('/admin')
+    if (req.files.Image) {
+      let image = req.files.Image
+      image.mv('./public/product-images/' + req.params.id + '.jpg')
+      }
+    
+  })  
 })
 
 module.exports = router;
