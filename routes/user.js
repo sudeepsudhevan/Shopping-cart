f33ee13a7c51bde2b17a4ff5a793679a8f2e92e4
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-21 18:45:04 +0530

    render cart page and add to cart

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers = require('../helpers/user-helpers');
 
+
 const verifyLogin = (req, res, next) => {
   if(req.session.loggedIn){
     next();
@@ -12,11 +13,15 @@ const verifyLogin = (req, res, next) => {
 }
 
 /* GET home page. */
-router.get('/', function (req, res, next) { // This is the root route
+router.get('/', async function (req, res, next) { // This is the root route
   let user = req.session.user;
   console.log(user);
+  let cartCount = null;
+  if(req.session.user){
+  cartCount = await userHelpers.getCartCount(req.session.user._id);
+  }
   productHelpers.getAllProducts().then((products) => {
-    res.render('user/view-products', { products, user }); //render is used to render the page, here it is view-products page
+    res.render('user/view-products', { products, user,cartCount }); //render is used to render the page, here it is view-products page
   });
   
 });
@@ -34,6 +39,9 @@ router.get('/signup', (req, res) => {
 router.post('/signup', (req, res) => {
   userHelpers.doSignup(req.body).then((response) => {
     console.log(response);
+    req.session.loggedIn = true;
+    req.session.user = response;
+
     res.redirect('/login'); //redirect to login page
     
   });
@@ -58,8 +66,18 @@ router.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-router.get('/cart',verifyLogin, (req, res) => {
-  res.render('user/cart');
+router.get('/cart',verifyLogin, async(req, res) => {
+  let products =await userHelpers.getCartProducts(req.session.user._id);
+  console.log(products);
+  res.render('user/cart',{products,user:req.session.user});
+});
+
+router.get('/add-to-cart/:id',verifyLogin, (req, res) => {
+  console.log("api call");
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.json({status:true})
+    
+  })
 });
 
 
