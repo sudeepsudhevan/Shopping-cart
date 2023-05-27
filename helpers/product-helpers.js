b3b7debbae26d111de416c51ec0a0af4c3730986
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-27 16:01:15 +0530

    product admin helper funstion

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -52,7 +52,83 @@ module.exports = {
                     resolve()
                 })
             })
-        }
+        },
+        getCartCount:(userId)=>{
+            return new Promise(async(resolve,reject)=>{
+                let count=0;
+                let cart=await db.get().collection(collection.CART_COLLECTIONS).findOne({user:ObjectId(userId)})
+                if(cart){
+                    count=cart.products.length
+                }
+                resolve(count)
+            })
+        },
+        getAllOrders:(userId)=>{
+            return new Promise(async(resolve,reject)=>{
+                let orders=await db.get().collection(collection.ORDER_COLLECTIONS).find().toArray()
+                resolve(orders)
+            })
+        },
+        getOrderProducts:(orderId)=>{
+            return new Promise(async(resolve,reject)=>{
+                let orderItems=await db.get().collection(collection.ORDER_COLLECTIONS).aggregate([
+                    {
+                        $match:{_id:ObjectId(orderId)}
+                    },
+                    {
+                        $unwind:'$products'
+                    },
+                    {
+                        $project:{
+                            item:'$products.item',
+                            quantity:'$products.quantity'
+                        }
+                    },
+                    {
+                        $lookup:{
+                            from:collection.PRODUCT_COLLECTIONS,
+                            localField:'item',
+                            foreignField:'_id',
+                            as:'product'
+                        }
+                    },
+                    {
+                        $project:{
+                            item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
+                        }
+                    }
+                ]).toArray()
+                console.log(orderItems);
+                resolve(orderItems)
+            })
+        },
+        changeStatus:(orderId,orderStatus)=>{
+            return new Promise((resolve,reject)=>{
+                db.get().collection(collection.ORDER_COLLECTIONS).updateOne({_id:ObjectId(orderId)},
+                {
+                    $set:{
+                        status:orderStatus
+                    }
+                }).then(()=>{
+                    resolve()
+                })
+            })
+        },
+        getAllUsers:()=>{
+            return new Promise(async(resolve,reject)=>{
+                let users=await db.get().collection(collection.USER_COLLECTIONS).find().toArray()
+                resolve(users)
+            })
+        },
+        getUserDetails:(userId)=>{
+            return new Promise((resolve,reject)=>{
+                db.get().collection(collection.USER_COLLECTIONS).findOne({_id:ObjectId(userId)}).then((user)=>{
+                    resolve(user)
+                })
+            })
+        },
+        
+
 
 }
      
